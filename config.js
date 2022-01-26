4f5c3290a8779d6c05adf88f09ac74d284c980f1
Cocopie <92080735+iCocopie@users.noreply.github.com>
2022-01-26 15:05:13 +0700

    Update config.js
    .

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ global.APIKeys = {
 	'https://zeks.me': 'Alphabott',
 }
 
-global.owner = ['62887435047326','918156874290']
+global.owner = ['628992029499','628992029499']
 global.packname = setting.botname
 global.author = setting.ownername
 global.alphaVersion = [2, 2143, 3]
